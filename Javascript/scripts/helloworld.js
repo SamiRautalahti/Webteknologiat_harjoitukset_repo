8aa4ff4ac3f665c048d3aaf2be514e90fc64406b
SamiRautalahti <sami.rautalahti@student.lab.fi>
2022-04-07 12:31:23 +0300

    Projektin päivitys

--- Javascript/scripts/helloworld.js
+++ Javascript/scripts/helloworld.js
@@ -1,8 +1,8 @@
-window.onload = function() {
+window.onload = function () {
     console.log("Tämä kirjoitetaan selaimen konsoliin");
 }
 
 function sayHelloTo(name) {
     alert("Hei " + name + "!");
-    console.log("sayHello-funktio suoritettu");
+    console.log("HelloWorld!");
 }
