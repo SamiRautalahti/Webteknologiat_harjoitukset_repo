67324adb7d0c271e4c09c023807fef80fdcf485a
SamiRautalahti <sami.rautalahti@student.lab.fi>
2022-04-21 17:16:49 +0300

    Projektin päivitys

--- Javascript/scripts/carousel.js
+++ Javascript/scripts/carousel.js
@@ -1,12 +1,12 @@
 let story_array = [
-    { "title": "Aaaaa", "content": "Aaaa aaaaaa aaaaa, aaa aaa a aa a.", "src": 'images/dog1.jpg' },
-    { "title": "Bbbbb", "content": "Bbbb bbbbbb bbbbb, bbb bbb b bb b.", "src": 'images/dog2.jpg' },
-    { "title": "Ccccc", "content": "Cccc cccccc ccccc, ccc ccc c cc c.", "src": 'images/dog3.jpg' },
-    { "title": "Ddddd", "content": "Dddd dddddd ddddd, ddd ddd d dd d.", "src": 'images/dog4.jpg' },
-    { "title": "Eeeee", "content": "Eeee eeeeee eeeee, eee eee e ee e.", "src": 'images/dog5.jpg' },
-    { "title": "Fffff", "content": "Ffff ffffff fffff, fff fff f ff f.", "src": 'images/dog6.jpg' },
-    { "title": "Ggggg", "content": "Ffff ffffff fffff, fff fff f ff f.", "src": 'images/dog7.jpg' },
-    { "title": "Hhhhh", "content": "Ffff ffffff fffff, fff fff f ff f.", "src": 'images/dog8.jpg' },
+    { "title": "Aaaaa", "content": "Aaaa aaaaaa aaaaa, aaa aaa a aa a.", "src": "images/dog1.jpg" },
+    { "title": "Bbbbb", "content": "Bbbb bbbbbb bbbbb, bbb bbb b bb b.", "src": "images/dog2.jpg" },
+    { "title": "Ccccc", "content": "Cccc cccccc ccccc, ccc ccc c cc c.", "src": "images/dog3.jpg" },
+    { "title": "Ddddd", "content": "Dddd dddddd ddddd, ddd ddd d dd d.", "src": "images/dog4.jpg" },
+    { "title": "Eeeee", "content": "Eeee eeeeee eeeee, eee eee e ee e.", "src": "images/dog5.jpg" },
+    { "title": "Fffff", "content": "Ffff ffffff fffff, fff fff f ff f.", "src": "images/dog6.jpg" },
+    { "title": "Ggggg", "content": "Ffff ffffff fffff, fff fff f ff f.", "src": "images/dog7.jpg" },
+    { "title": "Hhhhh", "content": "Ffff ffffff fffff, fff fff f ff f.", "src": "images/dog8.jpg" },
 ];
 
 let index;
