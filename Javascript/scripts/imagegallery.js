de5407c00fa4a14401c79250e65672c5889e0add
Sami Rautalahti <Sami.Rautalahti@student.lab.fi>
2022-04-21 19:55:47 +0300

    Projektin päivitys

--- Javascript/scripts/imagegallery.js
+++ Javascript/scripts/imagegallery.js
@@ -1,3 +1,5 @@
 window.onload = function () {
     document.body.setAttribute("style", "background-image: url('images/taustakuva.jpg')")
 }
+
+$('#image-container').removeClass('grid-container').addClass('list-container');
