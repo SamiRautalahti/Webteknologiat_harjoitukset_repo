8103803c10533055498e7b25c677e0a8962be539
Sami Rautalahti <Sami.Rautalahti@student.lab.fi>
2022-04-21 21:42:03 +0300

    Projektin päivitys

--- Javascript/scripts/imagegallery.js
+++ Javascript/scripts/imagegallery.js
@@ -2,4 +2,18 @@ window.onload = function () {
     document.body.setAttribute("style", "background-image: url('images/taustakuva.jpg')")
 }
 
-$('#image-container').removeClass('grid-container').addClass('list-container');
+
+var element = "";
+
+function naytaGrid() {
+    element = document.getElementById("image-container");
+    element.classList.remove("flex-container");
+    element.classList.add("grid-container");
+}
+
+function naytaFlex() {
+    element = document.getElementById("image-container");
+    element.classList.remove("grid-container");
+    element.classList.add("flex-container");
+
+}
