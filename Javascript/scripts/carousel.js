ec9153735a6d98436ee9acf4b89a891c848e1021
Sami Rautalahti <Sami.Rautalahti@student.lab.fi>
2022-04-22 08:25:40 +0300

    Projektin päivitys

--- Javascript/scripts/carousel.js
+++ Javascript/scripts/carousel.js
@@ -3,14 +3,14 @@ window.onload = function () {
 }
 
 let story_array = [
-    { "title": "Aaaaa", "content": "Aaaa aaaaaa aaaaa, aaa aaa a aa a.", "src": "images/dog1.jpg" },
-    { "title": "Bbbbb", "content": "Bbbb bbbbbb bbbbb, bbb bbb b bb b.", "src": "images/dog2.jpg" },
-    { "title": "Ccccc", "content": "Cccc cccccc ccccc, ccc ccc c cc c.", "src": "images/dog3.jpg" },
-    { "title": "Ddddd", "content": "Dddd dddddd ddddd, ddd ddd d dd d.", "src": "images/dog4.jpg" },
-    { "title": "Eeeee", "content": "Eeee eeeeee eeeee, eee eee e ee e.", "src": "images/dog5.jpg" },
-    { "title": "Fffff", "content": "Ffff ffffff fffff, fff fff f ff f.", "src": "images/dog6.jpg" },
-    { "title": "Ggggg", "content": "Ffff ffffff fffff, fff fff f ff f.", "src": "images/dog7.jpg" },
-    { "title": "Hhhhh", "content": "Ffff ffffff fffff, fff fff f ff f.", "src": "images/dog8.jpg" },
+    { "title": "Eka koira", "content": "Aaaa aaaaaa aaaaa, aaa aaa a aa a.", "src": "images/dog1.jpg" },
+    { "title": "Toka koira", "content": "Bbbb bbbbbb bbbbb, bbb bbb b bb b.", "src": "images/dog2.jpg" },
+    { "title": "Kolmas koira", "content": "Cccc cccccc ccccc, ccc ccc c cc c.", "src": "images/dog3.jpg" },
+    { "title": "Neljäs koira", "content": "Dddd dddddd ddddd, ddd ddd d dd d.", "src": "images/dog4.jpg" },
+    { "title": "Viides koira", "content": "Eeee eeeeee eeeee, eee eee e ee e.", "src": "images/dog5.jpg" },
+    { "title": "Kuudes koira", "content": "Ffff ffffff fffff, fff fff f ff f.", "src": "images/dog6.jpg" },
+    { "title": "Seitsemäs koira", "content": "Ffff ffffff fffff, fff fff f ff f.", "src": "images/dog7.jpg" },
+    { "title": "Kahdeksas koira", "content": "Ffff ffffff fffff, fff fff f ff f.", "src": "images/dog8.jpg" },
 ];
 
 let index;
