ac67139f51184a0fdbdcd970983a5e4477c397f9
Sami Rautalahti <Sami.Rautalahti@student.lab.fi>
2022-04-28 17:30:36 +0300

    Projektin päivitys

--- Javascript/scripts/jokes.js
+++ Javascript/scripts/jokes.js
@@ -24,11 +24,11 @@ $("#btn").click(function () {
                 let data = '<div class="accordion-item">'
                     + '<h2 class="accordion-header" id="flush-heading' + index + '">'
                     + '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse' + index + '" aria-expanded="false" aria-controls="flush-collapse' + index + '">'
-                    + 'Vitsi' + index + 
+                    + 'Vitsi' + index 
                     + '</button>'
                     + '</h2>'
                     + '<div id="flush-collapse' + index + '" class="accordion-collapse collapse" aria-labelledby="flush-heading' + index + '" data-bs-parent="#accordionFlushExample">'
-                    + response.value.joke +
+                    + response.value.joke
                     + '</div>';
                 document.getElementById("accordionFlushExample").innerHTML += data;
                 $("#gif").hide();
